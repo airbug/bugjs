f2e466c45afae9078de2862d5e26a5c5f15d00a2
Sung Choi <m.sung.choi@gmail.com>
2013-11-27 17:03:15 -0500

    Update. Updates ace ext files to work with bugpack.

--- external/ace/js/src/ext/ext-spellcheck.js
+++ external/ace/js/src/ext/ext-spellcheck.js
@@ -1,67 +1,104 @@
-ace.define('ace/ext/spellcheck', ['require', 'exports', 'module' , 'ace/lib/event', 'ace/editor', 'ace/config'], function(require, exports, module) {
+//-------------------------------------------------------------------------------
+// Annotations
+//-------------------------------------------------------------------------------
 
-var event = require("../lib/event");
+//@Package('aceext')
 
-exports.contextMenuHandler = function(e){
-    var host = e.target;
-    var text = host.textInput.getElement();
-    if (!host.selection.isEmpty())
-        return;
-    var c = host.getCursorPosition();
-    var r = host.session.getWordRange(c.row, c.column);
-    var w = host.session.getTextRange(r);
+//@Export('Spellcheck')
 
-    host.session.tokenRe.lastIndex = 0;
-    if (!host.session.tokenRe.test(w))
-        return;
-    var PLACEHOLDER = "\x01\x01";
-    var value = w + " " + PLACEHOLDER;
-    text.value = value;
-    text.setSelectionRange(w.length + 1, w.length + 1);
-    text.setSelectionRange(0, 0);
+//@Require('ace.Ace')
 
-    var afterKeydown = false;
-    event.addListener(text, "keydown", function onKeydown() {
-        event.removeListener(text, "keydown", onKeydown);
-        afterKeydown = true;
-    });
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
 
-    host.textInput.setInputHandler(function(newVal) {
-        console.log(newVal , value, text.selectionStart, text.selectionEnd)
-        if (newVal == value)
-            return '';
-        if (newVal.lastIndexOf(value, 0) === 0)
-            return newVal.slice(value.length);
-        if (newVal.substr(text.selectionEnd) == value)
-            return newVal.slice(0, -value.length);
-        if (newVal.slice(-2) == PLACEHOLDER) {
-            var val = newVal.slice(0, -2);
-            if (val.slice(-1) == " ") {
-                if (afterKeydown)
-                    return val.substring(0, text.selectionEnd);
-                val = val.slice(0, -1);
-                host.session.replace(r, val);
-                return "";
+var bugpack = require('bugpack').context();
+
+//-------------------------------------------------------------------------------
+// Common Modules
+//-------------------------------------------------------------------------------
+
+var ace = bugpack.require('ace.Ace');
+
+//-------------------------------------------------------------------------------
+//
+//-------------------------------------------------------------------------------
+
+var Spellcheck = {};
+Spellcheck.load = function() {
+
+    ace.define('ace/ext/spellcheck', ['require', 'exports', 'module' , 'ace/lib/event', 'ace/editor', 'ace/config'], function(require, exports, module) {
+        
+        var event = require("../lib/event");
+        
+        exports.contextMenuHandler = function(e){
+            var host = e.target;
+            var text = host.textInput.getElement();
+            if (!host.selection.isEmpty())
+                return;
+            var c = host.getCursorPosition();
+            var r = host.session.getWordRange(c.row, c.column);
+            var w = host.session.getTextRange(r);
+        
+            host.session.tokenRe.lastIndex = 0;
+            if (!host.session.tokenRe.test(w))
+                return;
+            var PLACEHOLDER = "\x01\x01";
+            var value = w + " " + PLACEHOLDER;
+            text.value = value;
+            text.setSelectionRange(w.length + 1, w.length + 1);
+            text.setSelectionRange(0, 0);
+        
+            var afterKeydown = false;
+            event.addListener(text, "keydown", function onKeydown() {
+                event.removeListener(text, "keydown", onKeydown);
+                afterKeydown = true;
+            });
+        
+            host.textInput.setInputHandler(function(newVal) {
+                console.log(newVal , value, text.selectionStart, text.selectionEnd)
+                if (newVal == value)
+                    return '';
+                if (newVal.lastIndexOf(value, 0) === 0)
+                    return newVal.slice(value.length);
+                if (newVal.substr(text.selectionEnd) == value)
+                    return newVal.slice(0, -value.length);
+                if (newVal.slice(-2) == PLACEHOLDER) {
+                    var val = newVal.slice(0, -2);
+                    if (val.slice(-1) == " ") {
+                        if (afterKeydown)
+                            return val.substring(0, text.selectionEnd);
+                        val = val.slice(0, -1);
+                        host.session.replace(r, val);
+                        return "";
+                    }
+                }
+        
+                return newVal;
+            });
+        };
+        var Editor = require("../editor").Editor;
+        require("../config").defineOptions(Editor.prototype, "editor", {
+            spellcheck: {
+                set: function(val) {
+                    var text = this.textInput.getElement();
+                    text.spellcheck = !!val;
+                    if (!val)
+                        this.removeListener("nativecontextmenu", exports.contextMenuHandler);
+                    else
+                        this.on("nativecontextmenu", exports.contextMenuHandler);
+                },
+                value: true
             }
-        }
-
-        return newVal;
-    });
+        });
+        
+        });
+        
+        
 };
-var Editor = require("../editor").Editor;
-require("../config").defineOptions(Editor.prototype, "editor", {
-    spellcheck: {
-        set: function(val) {
-            var text = this.textInput.getElement();
-            text.spellcheck = !!val;
-            if (!val)
-                this.removeListener("nativecontextmenu", exports.contextMenuHandler);
-            else
-                this.on("nativecontextmenu", exports.contextMenuHandler);
-        },
-        value: true
-    }
-});
 
-});
+//-------------------------------------------------------------------------------
+// Exports
+//-------------------------------------------------------------------------------
 
+bugpack.export('aceext.Spellcheck', Spellcheck);
